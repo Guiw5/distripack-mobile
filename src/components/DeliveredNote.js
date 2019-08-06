07cb7801626916f40acf230951a811d8ae37eb4e
Guido Wagner <wagner.guido.5@gmail.com>
2019-08-06 01:38:24 -0300

    fix imports error

--- src/components/DeliveredNote.js
+++ src/components/DeliveredNote.js
@@ -1,10 +1,13 @@
+import React from 'react'
 import moment from 'moment'
 import { myColors } from '../lib/commons'
+import { View } from 'react-native'
+import { Text } from 'react-native-elements'
 
-const DeliveredNote = props => (
+const DeliveredNote = deliveredAt => (
   <View style={{ alignItems: 'center', justifyContent: 'center' }}>
     <Text style={{ color: myColors.danger }}>
-      {`Pedido entregado el día ${moment(props.deliveredAt)
+      {`Pedido entregado el día ${moment(deliveredAt)
         .format('dddd, DD/MM HH:mm')
         .toUpperCase()}hs`}
     </Text>
