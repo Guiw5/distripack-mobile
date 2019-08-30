a01052c8b6e7a4a07deb594d31d37d7921a6220f
Guido Wagner <wagner.guido.5@gmail.com>
2019-08-30 02:57:12 -0300

    adding Sentry (crash reports)

--- App.js
+++ App.js
@@ -5,6 +5,12 @@ import { configureStore } from './src/store/configureStore'
 import { initApp } from './src/store/actions'
 import { useScreens } from 'react-native-screens'
 import Drawer from './src/navigation/MainDrawer'
+import Sentry from 'sentry-expo'
+
+Sentry.enableInExpoDevelopment = true
+Sentry.config(
+  'https://a4ef8c9c874d4e45b0143b709735d2b7@sentry.io/1540156'
+).install()
 
 useScreens()
 
